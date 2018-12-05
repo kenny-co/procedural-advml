ef5cb3e4473f0fd539d6f34edf999e8009e767f4
Kenneth Co <kenneth.co16@imperial.ac.uk>
2018-12-05 11:20:44 +0000

    Minor fixes Add comments for widget Fix Python 2 division for Perlin Tightened plot

--- procedural_noise.py
+++ procedural_noise.py
@@ -1,11 +1,13 @@
+from __future__ import division
+
 import numpy as np
 from noise import pnoise2
 
 # Normalize vector
 def normalize(vec):
-    max = np.amax(vec)
-    min  = np.amin(vec)
-    return (vec - min) / (max - min)
+    vmax = np.amax(vec)
+    vmin  = np.amin(vec)
+    return (vec - vmin) / (vmax - vmin)
 
 # Generate Perlin noise with sine function mapping
 # Assumes original image has shape (x_dim, y_dim, 3)
@@ -16,7 +18,7 @@ def perlin(x_dim, y_dim, period, octave, freq_sine):
     for x in range(x_dim):
         for y in range(y_dim):
             noise[x][y] = pnoise2(x / period, y / period, octaves = octave, lacunarity = 2)
-    
+            
     # Preprocessing and sine function color map
     noise = normalize(noise)
     noise = np.sin(noise * freq_sine * np.pi)
